a7371657c1ed9b273964c9a70f8a1ca0e5fee78d
sudaminis <sudamini.js@gmail.com>
2023-02-14 19:01:48 -0500

    Leetcode DSA commit

---
+++ src/main/java/techniques/treesgraphs/TreeNode.java
@@ -0,0 +1,12 @@
+package techniques.treesgraphs;
+
+public class TreeNode {
+    private int val;
+    private TreeNode leftNode;
+    private TreeNode rightNode;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+}
